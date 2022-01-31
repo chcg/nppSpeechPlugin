46459aa60f7412d016c0de5b07720ee57b58b806
VinsWorldcom <vin@vinsworld.com>
2022-01-31 10:08:19 -0500

    updates for Notepad++ 8.2.2

--- resource.h
+++ resource.h
@@ -20,6 +20,6 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define VER_RELEASE 1
+#define VER_RELEASE 2 
 #define VER_BUILD   1
 #define VER_STRING  STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_RELEASE) "." STR(VER_BUILD)
